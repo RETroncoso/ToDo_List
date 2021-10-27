f64812d6ab2d3e11e9dd8b810c031f7f53b8c2f1
Rodrigo Troncoso <rodrigo-rx@hotmail.com>
2021-10-27 20:14:48 -0300

    App finalizada

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const app = document.querySelector('#app');
 app.innerHTML = `
     <div class="todos">
         <div class="todos-header">
-            <h3 class="todos-title">TODO NUC</h3>
+            <h3 class="todos-title">PENDIENTES</h3>
             <div>
-                <p>Tienes <span class="todos-count"></span> Items por hacer!! </p>
+                <p>Tienes <span class="todos-count"></span> Item/s por hacer!! </p>
                 <button type="button" class="todos-clear" style="display:none;" >Borra Completados</button>
             </div>
         </div>
         <form class="todos-form" name="todos">
-            <input type="text" placeholder="Que vas hacer KING????" name="todo">
-            <small>Escribi algo KING!!!!</small>
+            <input type="text" placeholder="Ingresa tu tarea a realizar" name="todo">
+            <small>No se escribió nada</small>
         </form>
         <ul class="todos-list">
         </ul>
@@ -22,7 +22,7 @@ const saveInLocalStorage = (todos) => {
   localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-//?Selectores
+//Selectores
 
 const root = document.querySelector('.todos');
 const list = root.querySelector('.todos-list');
@@ -31,12 +31,14 @@ const clear = root.querySelector('.todos-clear');
 const form = document.forms.todos;
 const input = form.elements.todo;
 
+
+//Lista de ToDos
 let state = JSON.parse(localStorage.getItem("todos")) || [];
 
-//?HANDLERS VIEW
+//FUNCIONES VISUALES
 const renderTodos = (todos) => {
-  let listString = ''; //Se arranca con un string vacio
-  todos.forEach((todo, index) => { //La idea es que con cada iteracion se agregue ese string al string principal
+  let listString = '';
+  todos.forEach((todo, index) => {
     listString += `
         <li data-id="${index}"${todo.complete ? ' class="todos-complete"' : ""}>
             <input type="checkbox"${todo.complete ? ' checked' : ""}>
@@ -45,24 +47,24 @@ const renderTodos = (todos) => {
         </li>
       `;
   });
-  list.innerHTML = listString; //Le decimos a la lista que su innerHTML va a ser listString
+  list.innerHTML = listString;
   clear.style.display = todos.filter((todo) => todo.complete).length ? "block" : "none";
   count.innerText = todos.filter((todo) => !todo.complete).length;
 };
 
-//?HANDLERS LOGIC
+//FUNCIONES LOGICAS
 
 //Add ToDo
 const addTodo = (e) => {
-  e.preventDefault(); //Como el evento es submit, prevenimos la recarga
-  const label = input.value.trim(); //Le pasamos un trim (sacamos espacios en blanco al principio y al final)
+  e.preventDefault();
+  const label = input.value.trim();
   const complete = false;
   if (label.length === 0) {
     form.classList.add('error');
-    return; //Si el input esta vacio, le agrego la clase error al form para que aoparezca el msje (Diplayeado en css). Y retornamos para que salga de la funcion
+    return; //
   }
   form.classList.remove('error');
-  state = [ //Aca creamos el objeto. Copio todo el array con un spread operator y agrego un objeto nuevo con label y complete
+  state = [
     ...state,
     {
       label,
@@ -70,21 +72,15 @@ const addTodo = (e) => {
     },
   ];
 
-  console.log(state);
-
-
-  //RENDERIZADO DE LOS TODOS
-  renderTodos(state); //Renderizamos el Todo pasandole el state a la funcion renderTodos
+  renderTodos(state);
   saveInLocalStorage(state);
-  input.value = ''; // Limpiamos el input
+  input.value = '';
 };
 
-//Update todo
+//COMPLETE TODO
 
 const updateTodo = ({target}) => {
-  //Obtenemos el data-id atributo
   const id = parseInt(target.parentNode.dataset.id);
-  //Asignar valor booleano al complete
   const complete = target.checked;
 
   state = state.map((todo, index) => {
@@ -102,7 +98,7 @@ const updateTodo = ({target}) => {
   saveInLocalStorage(state);
 };
 
-//Edit ToDo
+//EDITAR TODO
 const editTodo = ({target}) => {
   if (target.nodeName.toLowerCase() !== "span") {
     return;
@@ -147,22 +143,22 @@ const editTodo = ({target}) => {
   input.focus();
 }
 
-//Delete ToDo
+//BORRAR TODO/S
 const deleteTodo = ({target}) => {
   if (target.nodeName.toLowerCase() !== "button") {
     return;
   }
   const id = parseInt(target.parentNode.dataset.id);
   const label = target.previousElementSibling.innerText;
-  if (window.confirm(`Estas a punto de borrar ${label}, ok???`)) {
-    state = state.filter((todo, index) => index !== id); //Devolveme todo index que no sea igual al id
+  if (window.confirm(`Estas a punto de borrar "${label}", ok???`)) {
+    state = state.filter((todo, index) => index !== id);
     renderTodos(state);
     saveInLocalStorage(state);
   }
 };
 
 const clearCompletes = () => {
-  const todoCompletes = state.filter((todo) => todo.complete).length; //Tiene un return implicito. Me devuelve un array con todos los valores del state con complete true. A ese array, le hacemos un length
+  const todoCompletes = state.filter((todo) => todo.complete).length;
   if (todoCompletes === 0) {
     return;
   }
@@ -173,7 +169,7 @@ const clearCompletes = () => {
   }
 };
 
-//?ENTRY POINT - PUNTO DE ENTRADA A LA APP ---- INICIALIZADOR
+//DECLARACION DE EVENTOS E INICIALIZADOR DE ESTADO
 
 function init() {
   renderTodos(state);
@@ -184,5 +180,5 @@ function init() {
   clear.addEventListener('click', clearCompletes);
 }
 
-//RUN THE APPPPPPPP!!!!!!!!! BE NUCBER!!!!
+//INICIALIZADOR DE LA APP
 init();
